e94a2564e044140e65ced185336fd522fb1f304e
Luizcsf <luizorientador85@outlook.com>
2022-07-25 21:20:46 -0300

    First commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,20 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+
+
+
+    public Gato(String nome) {
+        this.nome = nome;
+    }
+
+    public Gato(){
+
+    }
+}
